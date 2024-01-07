9020f967f109e437bc928fd693f8ea5c7e33a537
ascot20 <danielotchere18@gmail.com>
2024-01-07 14:24:45 +0000

    updated e2e test

--- cypress/e2e/pokemon_app.cy.js
+++ cypress/e2e/pokemon_app.cy.js
@@ -5,11 +5,11 @@ describe('Pokedex', function () {
     cy.contains(
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
+  })
 
-    it('navigate from main page to venusaur', function(){
-      cy.visit('http://localhost:5000')
-      cy.contains('venusaur').click()
-      cy.contains('chlorophyll')
-    })
+  it('navigate from main page to venusaur', function(){
+    cy.visit('http://localhost:5000')
+    cy.contains('venusaur').click()
+    cy.contains('chlorophyll')
   })
 })
